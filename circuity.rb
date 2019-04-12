30b69de01d520945ed1713fd79ba8e0a3c0d492c
Leonard Collins <leonard.s.collins@gmail.com>
2019-04-12 11:06:44 -0400

    initial commit

---
+++ circuity.rb
@@ -0,0 +1,15 @@
+class Circuitry < Formula
+  desc "The CLI tools for building circuitry applications"
+  homepage "https://www.getcircuitry.com"
+  url "https://github.com/TheHipbot/weather/raw/master/archive/weather-1.0.0.tar.gz"
+  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  version "1.0.0"
+
+  depends_on "curl"
+
+  bottle :unneeded
+
+  def install
+    bin.install "circuitry"
+  end
+end
