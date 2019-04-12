cac0912a746f603d08a37630bb2b6aea43e6e6fa
Leonard Collins <leonard.s.collins@gmail.com>
2019-04-12 12:11:30 -0400

    updates formula

--- circuity.rb
+++ circuity.rb
@@ -1,15 +1,13 @@
 class Circuitry < Formula
   desc "The CLI tools for building circuitry applications"
   homepage "https://www.getcircuitry.com"
-  url "https://github.com/TheHipbot/weather/raw/master/archive/weather-1.0.0.tar.gz"
-  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  url "https://www.getcircuitry.com/maven/com/circuitry/tools/component-creator/1.0/component-creator-1.0.zip"
+  sha256 "5018dcecddc2e3406d25072336bf68e7609006d70967e849757f2eb7a8ee8d7d"
   version "1.0.0"
 
-  depends_on "curl"
-
-  bottle :unneeded
-
   def install
-    bin.install "circuitry"
+    inreplace "brew/circuitry", "##PREFIX##", "#{prefix}"
+    prefix.install "create-tool.jar"
+    bin.install "brew/circuitry"
   end
 end
