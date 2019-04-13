9cf0958ad345f73dabfd17edc8242a0705401eed
Leonard Collins <leonard.s.collins@gmail.com>
2019-04-12 20:21:00 -0400

    version - 1.0

--- circuitry.rb
+++ circuitry.rb
@@ -2,8 +2,8 @@
 class Circuitry < Formula
   desc "The CLI tools for building circuitry applications"
   homepage "https://www.getcircuitry.com"
-  url "https://www.getcircuitry.com/maven/com/circuitry/tools/project-creator/1.0.zip"
-  sha256 "a9969f7a333b1c416d1d0ceeab2b9513a43f9f100742c3327ed8c3810dfb244d"
+  url "https://www.getcircuitry.com/maven/com/circuitry/tools/project-creator/1.0/project-creator-1.0.zip"
+  sha256 "11be30a2720f42e6c864f5f7ae8668140bdedda804c7049b35215216b7a12f58"
   version "1.0"
 
   def install
