73e896567ec105b9272be1c1c87f4d4253cfcbe8
Leonard Collins <leonard.s.collins@gmail.com>
2019-04-12 20:09:57 -0400

    version - 1.0

--- circuitry.rb
+++ circuitry.rb
@@ -3,7 +3,7 @@ class Circuitry < Formula
   desc "The CLI tools for building circuitry applications"
   homepage "https://www.getcircuitry.com"
   url "https://www.getcircuitry.com/maven/com/circuitry/tools/component-creator/1.0/component-creator-1.0.zip"
-  sha256 "4db3cce432efe29cec75e89fa246196ec5a5108dd810e1845f0660ea751f1eaf"
+  sha256 "55862a51831921254e178b8bfe67d52c8a366b5ff734abd24c5257547cfd5e98"
   version "1.0"
 
   def install
